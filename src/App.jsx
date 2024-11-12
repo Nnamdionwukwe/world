552d40a49b146f0c61a1283dea59a6cbfeac4bc6
Nnamdionwukwe <82799671+Nnamdionwukwe@users.noreply.github.com>
2024-11-12 16:23:12 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -155,7 +155,7 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route
-            path="HomePages"
+            path="/"
             element={
               <HomePages
                 handleSelectId={handleSelectId}
