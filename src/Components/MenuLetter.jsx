a26403fcbd9ec9fe11fed3db777a51e757cbd065
Nnamdi Onwukwe <nnamdionwuke@gmail.com>
2024-11-19 01:13:48 +0100

    new

--- src/Components/MenuLetter.jsx
+++ src/Components/MenuLetter.jsx
@@ -3,14 +3,16 @@ import styles from "./MenuLetter.module.css";
 export default function MenuLetter() {
   return (
     <div className={styles.menuLetter}>
-      <h3>BROWSE OUR MENU</h3>
+      <div>
+        <h3>BROWSE OUR MENU</h3>
 
-      <div className={styles.div1}>
-        <div className={styles.div}></div>
-        <div className={styles.div2}></div>
+        <div className={styles.div1}>
+          <div className={styles.div}></div>
+          <div className={styles.div2}></div>
+        </div>
+
+        <h5>Food & Drinks</h5>
       </div>
-
-      <h5>Food & Drinks</h5>
     </div>
   );
 }
