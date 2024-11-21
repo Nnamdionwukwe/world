e9b87d5b6917212d0679076bbdb18f318eb7dde1
Nnamdi Onwukwe <nnamdionwuke@gmail.com>
2024-11-21 16:40:11 +0100

    new

--- src/Components/HomeFooter.jsx
+++ src/Components/HomeFooter.jsx
@@ -53,22 +53,24 @@ export default function HomeFooter({ cart }) {
       </div>
 
       <div className={styles.barcodeDiv}>
-        <h3 className={styles.stanforde}>Stanforde Laze</h3>
+        <div>
+          <h3 className={styles.stanforde}>Stanforde Laze</h3>
 
-        <div className={styles.independDiv}>
-          <div>
-            <i className="fa fa-map-marker" aria-hidden="true"></i>
-            <p>27 Independence Avanue,</p>
+          <div className={styles.independDiv}>
+            <div>
+              <i className="fa fa-map-marker" aria-hidden="true"></i>
+              <p>27 Independence Avanue,</p>
+            </div>
+
+            <p>Independence Layout Enugu</p>
           </div>
 
-          <p>Independence Layout Enugu</p>
-        </div>
-
-        <div className={styles.mailDiv}>
-          <i className="fa fa-envelope" aria-hidden="true"></i>
-          <a href="www.gmail.com">
-            <p>standfordelaze&#64;gmail.com</p>
-          </a>
+          <div className={styles.mailDiv}>
+            <i className="fa fa-envelope" aria-hidden="true"></i>
+            <a href="www.gmail.com">
+              <p>standfordelaze&#64;gmail.com</p>
+            </a>
+          </div>
         </div>
       </div>
 
