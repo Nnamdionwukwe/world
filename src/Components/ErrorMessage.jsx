539b0796762bdcb771a3684abb121d9c34ed29da
Nnamdi Onwukwe <nnamdionwuke@gmail.com>
2024-11-21 14:36:15 +0100

    new

--- src/Components/ErrorMessage.jsx
+++ src/Components/ErrorMessage.jsx
@@ -4,7 +4,7 @@ import styles from "./ErrorMessage.module.css";
 export default function ErrorMessage({ message }) {
   return (
     <h3 className={styles.message}>
-      Sorry {message} Dishes please check your network connection!!
+      Sorry {message} please check your network connection!!
     </h3>
   );
 }
