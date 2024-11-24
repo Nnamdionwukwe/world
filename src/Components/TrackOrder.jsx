6e66e74afef1ddf3b6379d4f6c62bfa55a493916
Nnamdi Onwukwe <nnamdionwuke@gmail.com>
2024-11-24 18:52:23 +0100

    new

--- src/Components/TrackOrder.jsx
+++ src/Components/TrackOrder.jsx
@@ -1,14 +1,27 @@
 import FooterHeader from "./FotterHeader";
 import PagesHeader from "./PagesHeader";
+import styles from "./TrackOrder.module.css";
 
 export default function TrackOrder(cart) {
   return (
-    <div>
+    <>
       <PagesHeader />
 
-      <div> </div>
+      <div className={styles.main}>
+        <div className={styles.mainSub}>
+          <div className={styles.mainTrack}>
+            <p>Track order</p>
+          </div>
+
+          <div className={styles.phoneDiv}>
+            <p>
+              Phone Number <span>*</span>
+            </p>
+          </div>
+        </div>
+      </div>
 
       <FooterHeader cart={cart} />
-    </div>
+    </>
   );
 }
